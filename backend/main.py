e35204e29e0b0bd35fbd6f006fb0607268dcfa8e
Nazmul Hossain <nazhoscse@gmail.com>
2026-02-11 11:34:02 -0500

    added the origin to allow the browser extention

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5200",
+        "https://github.com"
     ],
     allow_credentials=False,
     allow_methods=["*"],
